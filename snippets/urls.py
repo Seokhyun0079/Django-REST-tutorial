51cfaa71ebcdc272092d830a0bf42e3274b0daab
Seokhyun0079 <seokhyun0324@gmail.com>
2022-06-05 18:07:42 +0900

    Tutorial 4: Authentication & Permissions

--- snippets/urls.py
+++ snippets/urls.py
@@ -3,10 +3,12 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
+    path('', views.api_root),
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>', views.snippet_detail),
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- snippets/views.py
+++ snippets/views.py
@@ -17,6 +17,27 @@ from django.contrib.auth.models import User
 from snippets.serializers import UserSerializer
 from rest_framework import permissions
 from snippets.permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import renderers
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    print('test')
+    
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
 
 @csrf_exempt
 @api_view(['GET', 'POST'])
